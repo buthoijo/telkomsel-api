f3eec4da30dcaacf1402603443f43edc9d90d2bb
buthoijo <61355726+buthoijo@users.noreply.github.com>
2020-03-07 21:58:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require('./src/app');
 
 app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running at localhost:' + app.get('port'));
+app.listen(app.post('port'), function() {
+  console.log('Node app is running at localhost:' + app.post('port'));
 });
